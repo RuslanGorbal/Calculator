67c94761d115f315db64907678182f0d6f16a2ef
Ruslan <ruslangorbal719@gmail.com>
2020-05-24 22:46:40 +0300

    gg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'yourApp';
+  title = 'Angular Calculator';
+  public num1:number;
+  public num2:number;
+  public num3:number;
+
+  Dod(){
+    this.num3=this.num1+this.num2;
+  }
+  Vid(){
+    this.num3=this.num1-this.num2;
+  }
+  Mno(){
+    this.num3=this.num1*this.num2;
+  }
+  Dil(){
+    this.num3=this.num1/this.num2;
+  }
 }
+
